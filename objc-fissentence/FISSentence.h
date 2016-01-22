111a6cdd66399f083f2700d69c95b055df75cb41
joshmotley <joshkmotley@gmail.com>
2016-01-22 00:12:46 -0500

    create custom class that assembles components into string

---
+++ objc-fissentence/FISSentence.h
@@ -0,0 +1,13 @@
+//
+//  FISSentence.h
+//  objc-fissentence
+//
+//  Created by Joshua Motley on 1/21/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISSentence : NSObject
+
+@end
---
+++ objc-fissentence/FISSentence.m
@@ -0,0 +1,13 @@
+//
+//  FISSentence.m
+//  objc-fissentence
+//
+//  Created by Joshua Motley on 1/21/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISSentence.h"
+
+@implementation FISSentence
+
+@end
